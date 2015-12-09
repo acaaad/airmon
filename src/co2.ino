719e240b27c2fb156768e836bfbb5f54fe8e16d2
acaaad <auliarshad@gmail.com>
2015-12-09 22:33:21 +0700

    co2

--- src/co2.ino
+++ src/co2.ino
@@ -1,28 +1,23 @@
 #include <SoftwareSerial.h>
-#include "cozir.h"
 
-SoftwareSerial nss(3,2);
-COZIR czr(nss);
+SoftwareSerial nss(11,10);
 
 void setup()
 {
- Serial.begin(9600);
- delay(3000);
- //czr.SetOperatingMode(CZR_POLLING);
- //czr.SetOperatingMode(CZR_STREAMING);
+  Serial.begin(9600);
+  nss.begin(9600);
+  delay(3000);
 }
 
+void getStuff()
+{
+  if (nss.available())
+  {
+    Serial.write(nss.read());
+  }
+}
 void loop()
 {
- float t = czr.Celsius();
- float f = czr.Fahrenheit();
- float h = czr.Humidity();
- int c = czr.CO2();
- int digi = czr.GetDigiFilter();
-
- Serial.print("Celcius : ");Serial.println(t);
- Serial.print("Fahrenheit : ");Serial.println(f);
- Serial.print("Humidity : ");Serial.println(h);
- Serial.print("CO2 : ");Serial.println(c);
- Serial.print("Digital Filter : ");Serial.println(digi);
+  getStuff();
+  delay(2000);
 }
